b68c6778ff180cac4c89c6d36fa230e23bd62498
precious godwin <preciousgodwin@preciouss-MacBook-Air.local>
2024-05-29 20:59:49 -0600

    update account.html style

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+document.getElementById('exploreButton').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.querySelector('.landing h1').classList.add('zoom-out');
+    setTimeout(function() {
+      window.location.href = event.target.querySelector('a').href;
+    }, 600); // The timeout duration should match the animation duration
+  });
