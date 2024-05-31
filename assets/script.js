a47034feb9290e121469c624d18f19a1a39dec01
precious godwin <preciousgodwin@preciouss-MacBook-Air.local>
2024-05-30 23:21:52 -0600

    update userprofile.html and css

--- assets/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('exploreButton').addEventListener('click', function(event) {
-    event.preventDefault();
-    document.querySelector('.landing h1').classList.add('zoom-out');
-    setTimeout(function() {
-      window.location.href = event.target.querySelector('a').href;
-    }, 600); // The timeout duration should match the animation duration
-  });
