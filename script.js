11881003327485b14ca6adbc728264555420c0e9
precious godwin <preciousgodwin@preciouss-MacBook-Air.local>
2024-05-31 20:16:27 -0600

    update css audio

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 //   });
 
   // Set the date we're counting down to
-var countDownDate = new Date("May 31, 2024 15:37:25").getTime();
+var countDownDate = new Date("June 1, 2024 15:37:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -34,4 +34,3 @@ var x = setInterval(function() {
 }, 1000);
 
 
-// Welcome page js
